49f435bcadf27b164855068b7e8405be82087096
sisby-folk <nora.herrah.heartstrings@gmail.com>
2024-02-06 15:39:10 +1100

    improve structure data format

--- src/main/java/folk/sisby/antique_atlas/structure/StructureSummary.java
+++ src/main/java/folk/sisby/antique_atlas/structure/StructureSummary.java
@@ -1,5 +1,7 @@
 package folk.sisby.antique_atlas.structure;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
 import com.mojang.serialization.Dynamic;
 import folk.sisby.antique_atlas.AntiqueAtlas;
 import net.minecraft.nbt.NbtCompound;
@@ -12,73 +14,68 @@ import net.minecraft.structure.PoolStructurePiece;
 import net.minecraft.structure.StructurePiece;
 import net.minecraft.structure.StructurePieceType;
 import net.minecraft.structure.StructureStart;
+import net.minecraft.structure.pool.FeaturePoolElement;
 import net.minecraft.structure.pool.SinglePoolElement;
+import net.minecraft.structure.pool.StructurePoolElementType;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockBox;
-import net.minecraft.util.math.ChunkPos;
-import net.minecraft.world.gen.structure.StructureType;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
-public record StructureSummary(ChunkPos pos, StructureType<?> type, Collection<StructurePieceSummary> pieces, Collection<JigsawSummary> jigsaws) {
-    public static final String KEY_X = "x";
-    public static final String KEY_Z = "z";
-    public static final String KEY_ID = "id";
+public record StructureSummary(Collection<StructurePieceSummary> pieces) {
     public static final String KEY_BOX = "BB";
     public static final String KEY_PIECES = "pieces";
-    public static final String KEY_JIGSAWS = "jigsaws";
 
     public static StructureSummary fromStart(StructureStart start) {
-        return new StructureSummary(
-            start.getPos(),
-            start.getStructure().getType(),
-            start.getChildren().stream().map(StructurePieceSummary::fromPiece).toList(),
-            start.getChildren().stream().map(JigsawSummary::fromPiece).filter(Optional::isPresent).map(Optional::get).toList()
-        );
+        List<StructurePieceSummary> pieces = new ArrayList<>();
+        for (StructurePiece piece : start.getChildren()) {
+            pieces.add(JigsawSummary.tryFromPiece(piece).orElseGet(() -> StructurePieceSummary.fromPiece(piece)));
+        }
+        return new StructureSummary(pieces);
     }
 
     public static StructureSummary fromNbt(NbtCompound nbt) {
-        ChunkPos pos = new ChunkPos(nbt.getInt(KEY_X), nbt.getInt(KEY_Z));
-        StructureType<?> type = nbt.contains(KEY_ID) ? Registries.STRUCTURE_TYPE.get(new Identifier(nbt.getString(KEY_ID))) : null;
         Collection<StructurePieceSummary> pieces = new ArrayList<>();
         for (NbtElement pieceElement : nbt.getList(KEY_PIECES, NbtElement.STRING_TYPE)) {
-            pieces.add(StructurePieceSummary.fromNbt((NbtCompound) pieceElement));
+            if (((NbtCompound) pieceElement).getString(StructurePieceSummary.KEY_TYPE).equals(Registries.STRUCTURE_PIECE.getId(StructurePieceType.JIGSAW).toString())) {
+                pieces.add(JigsawSummary.fromNbt((NbtCompound) pieceElement));
+            } else {
+                pieces.add(StructurePieceSummary.fromNbt((NbtCompound) pieceElement));
+            }
         }
-        Collection<JigsawSummary> jigsaws = new ArrayList<>();
-        for (NbtElement jigsawElement : nbt.getList(KEY_JIGSAWS, NbtElement.STRING_TYPE)) {
-            jigsaws.add(JigsawSummary.fromNbt((NbtCompound) jigsawElement));
-        }
-        return new StructureSummary(pos, type, pieces, jigsaws);
+        return new StructureSummary(pieces);
     }
 
     public NbtCompound writeNbt(NbtCompound nbt) {
-        nbt.putInt(KEY_X, pos.x);
-        nbt.putInt(KEY_Z, pos.z);
-        if (type != null) nbt.putString(KEY_ID, Registries.STRUCTURE_TYPE.getId(type).toString());
         NbtList pieceList = new NbtList();
         for (StructurePieceSummary piece : pieces) {
             pieceList.add(piece.writeNbt(new NbtCompound()));
         }
         nbt.put(KEY_PIECES, pieceList);
-        NbtList jigsawList = new NbtList();
-        for (JigsawSummary jigsaw : jigsaws) {
-            jigsawList.add(jigsaw.writeNbt(new NbtCompound()));
-        }
-        nbt.put(KEY_JIGSAWS, jigsawList);
         return nbt;
     }
 
-    public record StructurePieceSummary(StructurePieceType type, BlockBox boundingBox) {
+    public static class StructurePieceSummary {
+        public static final String KEY_TYPE = "type";
+        public final StructurePieceType type;
+        public final BlockBox boundingBox;
+
+        public StructurePieceSummary(StructurePieceType type, BlockBox boundingBox) {
+            this.type = type;
+            this.boundingBox = boundingBox;
+        }
+
         public static StructurePieceSummary fromPiece(StructurePiece piece) {
             return new StructurePieceSummary(piece.getType(), piece.getBoundingBox());
         }
 
         public static StructurePieceSummary fromNbt(NbtCompound nbt) {
             return new StructurePieceSummary(
-                Registries.STRUCTURE_PIECE.get(new Identifier(nbt.getString(KEY_ID))),
+                Registries.STRUCTURE_PIECE.get(new Identifier(nbt.getString(KEY_TYPE))),
                 BlockBox.CODEC
                     .parse(NbtOps.INSTANCE, nbt.get("BB"))
                     .resultOrPartial(AntiqueAtlas.LOG::error)
@@ -87,44 +84,67 @@ public record StructureSummary(ChunkPos pos, StructureType<?> type, Collection<S
         }
 
         public NbtCompound writeNbt(NbtCompound nbt) {
-            nbt.putString(KEY_ID, Registries.STRUCTURE_PIECE.getId(type).toString());
+            nbt.putString(KEY_TYPE, Registries.STRUCTURE_PIECE.getId(type).toString());
             BlockBox.CODEC.encodeStart(NbtOps.INSTANCE, this.boundingBox).resultOrPartial(AntiqueAtlas.LOG::error).ifPresent(element -> nbt.put(KEY_BOX, element));
             return nbt;
         }
     }
 
-    public record JigsawSummary(Identifier id, BlockBox boundingBox, List<JigsawJunction> junctions) {
+    public static class JigsawSummary extends StructurePieceSummary {
+        public static final BiMap<String, StructurePoolElementType<?>> TYPE_KEYS = HashBiMap.create(Map.of(
+            "single", StructurePoolElementType.SINGLE_POOL_ELEMENT,
+            "feature", StructurePoolElementType.FEATURE_POOL_ELEMENT
+        ));
         public static final String KEY_JUNCTIONS = "junctions";
+        public final StructurePoolElementType<?> jigsawType;
+        public final Identifier id;
+        public final BlockBox boundingBox;
+        public final List<JigsawJunction> junctions;
 
-        public static Optional<JigsawSummary> fromPiece(StructurePiece piece) {
-            if (piece instanceof PoolStructurePiece poolPiece && poolPiece.getPoolElement() instanceof SinglePoolElement poolElement && poolElement.location.left().isPresent()) {
-                return Optional.of(new JigsawSummary(poolElement.location.left().orElseThrow(), piece.getBoundingBox(), poolPiece.getJunctions()));
+        public JigsawSummary(StructurePoolElementType<?> jigsawType, Identifier id, BlockBox boundingBox, List<JigsawJunction> junctions) {
+            super(StructurePieceType.JIGSAW, boundingBox);
+            this.jigsawType = jigsawType;
+            this.id = id;
+            this.boundingBox = boundingBox;
+            this.junctions = junctions;
+        }
+
+        public static Optional<StructurePieceSummary> tryFromPiece(StructurePiece piece) {
+            if (piece instanceof PoolStructurePiece poolPiece) {
+                if (poolPiece.getPoolElement() instanceof SinglePoolElement poolElement && poolElement.location.left().isPresent()) {
+                    return Optional.of(new JigsawSummary(StructurePoolElementType.SINGLE_POOL_ELEMENT, poolElement.location.left().orElseThrow(), piece.getBoundingBox(), poolPiece.getJunctions()));
+                } else if (poolPiece.getPoolElement() instanceof FeaturePoolElement poolElement && poolElement.feature.getKey().isPresent()) {
+                    return Optional.of(new JigsawSummary(StructurePoolElementType.FEATURE_POOL_ELEMENT, poolElement.feature.getKey().orElseThrow().getValue(), piece.getBoundingBox(), poolPiece.getJunctions()));
+                }
             }
             return Optional.empty();
         }
 
         public static JigsawSummary fromNbt(NbtCompound nbt) {
+            StructurePieceSummary baseSummary = StructurePieceSummary.fromNbt(nbt);
             List<JigsawJunction> junctions = new ArrayList<>();
             nbt.getList(KEY_JUNCTIONS, NbtElement.COMPOUND_TYPE).forEach(junctionElement -> junctions.add(JigsawJunction.deserialize(new Dynamic<>(NbtOps.INSTANCE, junctionElement))));
 
+            String idKey = TYPE_KEYS.keySet().stream().filter(nbt::contains).findFirst().orElseThrow();
+
             return new JigsawSummary(
-                new Identifier(nbt.getString(KEY_ID)),
-                BlockBox.CODEC
-                    .parse(NbtOps.INSTANCE, nbt.get("BB"))
-                    .resultOrPartial(AntiqueAtlas.LOG::error)
-                    .orElseThrow(() -> new IllegalArgumentException("Invalid boundingbox")),
+                TYPE_KEYS.get(idKey),
+                new Identifier(nbt.getString(idKey)),
+                baseSummary.boundingBox,
                 junctions
             );
         }
 
+        @Override
         public NbtCompound writeNbt(NbtCompound nbt) {
-            nbt.putString(KEY_ID, id.toString());
-            BlockBox.CODEC.encodeStart(NbtOps.INSTANCE, this.boundingBox).resultOrPartial(AntiqueAtlas.LOG::error).ifPresent(element -> nbt.put(KEY_BOX, element));
+            super.writeNbt(nbt);
+            String idKey = TYPE_KEYS.inverse().get(jigsawType);
+            nbt.putString(idKey, id.toString());
             NbtList junctionList = new NbtList();
             for (JigsawJunction junction : junctions) {
                 junctionList.add(junction.serialize(NbtOps.INSTANCE).getValue());
             }
-            nbt.put(KEY_JUNCTIONS, junctionList);
+            if (!junctionList.isEmpty()) nbt.put(KEY_JUNCTIONS, junctionList);
             return nbt;
         }
     }
--- src/main/java/folk/sisby/antique_atlas/structure/StructureSummaryState.java
+++ src/main/java/folk/sisby/antique_atlas/structure/StructureSummaryState.java
@@ -3,52 +3,88 @@ package folk.sisby.antique_atlas.structure;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtList;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.structure.StructureStart;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.PersistentState;
-import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.gen.structure.Structure;
+import net.minecraft.world.gen.structure.StructureType;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StructureSummaryState extends PersistentState {
     public static final String STATE_KEY = "antique_atlas_structure_summary";
     public static final String KEY_STRUCTURES = "structures";
+    public static final String KEY_X = "x";
+    public static final String KEY_Z = "z";
+    public static final String KEY_TYPE = "type";
+    public static final String KEY_STARTS = "starts";
+    public static final String KEY_SUMMARY = "summary";
 
-    private final Set<StructureSummary> structures;
+    private final Map<ChunkPos, Map<StructureKey, StructureSummary>> structures;
 
-    public StructureSummaryState(Set<StructureSummary> structures) {
+    public StructureSummaryState(Map<ChunkPos, Map<StructureKey, StructureSummary>> structures) {
         this.structures = structures;
     }
 
-    public void addStructure(World world, StructureStart start) {
-        structures.add(StructureSummary.fromStart(start));
-        markDirty();
+    public void addStructure(ServerWorld world, StructureStart start) {
+        RegistryKey<Structure> key = world.getRegistryManager().get(RegistryKeys.STRUCTURE).getKey(start.getStructure()).orElseThrow();
+        structures.computeIfAbsent(start.getPos(), p -> new HashMap<>()).computeIfAbsent(new StructureKey(key, start.getStructure().getType()), k -> {
+            markDirty();
+            return StructureSummary.fromStart(start);
+        });
     }
 
     @Override
     public NbtCompound writeNbt(NbtCompound nbt) {
-        NbtList structureList = new NbtList();
-        structures.forEach(summary -> structureList.add(summary.writeNbt(new NbtCompound())));
-        nbt.put(KEY_STRUCTURES, structureList);
+        Map<StructureKey, Map<ChunkPos, StructureSummary>> perStructure = new HashMap<>();
+        structures.forEach((pos, map) -> map.forEach((structure, summary) -> {
+            perStructure.computeIfAbsent(structure, p -> new HashMap<>()).put(pos, summary);
+        }));
+        NbtCompound structuresCompound = new NbtCompound();
+        perStructure.forEach((structure, map) -> {
+            NbtCompound structureCompound = new NbtCompound();
+            structureCompound.putString(KEY_TYPE, Registries.STRUCTURE_TYPE.getId(structure.type).toString());
+            NbtList startList = new NbtList();
+            map.forEach((pos, summary) -> {
+                NbtCompound startCompound = new NbtCompound();
+                startCompound.putInt(KEY_X, pos.x);
+                startCompound.putInt(KEY_Z, pos.z);
+                startCompound.put(KEY_SUMMARY, summary.writeNbt(new NbtCompound()));
+                startList.add(startCompound);
+            });
+            structureCompound.put(KEY_STARTS, startList);
+            structuresCompound.put(structure.key.getValue().toString(), structureCompound);
+        });
+        nbt.put(KEY_STRUCTURES, structuresCompound);
         return nbt;
     }
 
     public static StructureSummaryState readNbt(NbtCompound nbt) {
-        Set<StructureSummary> structures = new HashSet<>();
-        for (NbtElement structureElement : nbt.getList(KEY_STRUCTURES, NbtElement.COMPOUND_TYPE)) {
-            NbtCompound structureCompound = ((NbtCompound) structureElement);
-            structures.add(
-                StructureSummary.fromNbt(structureCompound)
-            );
+        Map<ChunkPos, Map<StructureKey, StructureSummary>> structures = new HashMap<>();
+        for (String structureId : nbt.getCompound(KEY_STRUCTURES).getKeys()) {
+            RegistryKey<Structure> key = RegistryKey.of(RegistryKeys.STRUCTURE, new Identifier(structureId));
+            StructureType<?> type = Registries.STRUCTURE_TYPE.get(new Identifier(nbt.getCompound(structureId).getString(KEY_TYPE)));
+            NbtList startList = nbt.getCompound(KEY_STRUCTURES).getList(structureId, NbtElement.COMPOUND_TYPE);
+            for (NbtElement startElement : startList) {
+                NbtCompound startCompound = (NbtCompound) startElement;
+                ChunkPos pos = new ChunkPos(startCompound.getInt(KEY_X), startCompound.getInt(KEY_Z));
+                StructureSummary summary = StructureSummary.fromNbt(startCompound.getCompound(KEY_SUMMARY));
+                structures.computeIfAbsent(pos, p -> new HashMap<>()).put(new StructureKey(key, type), summary);
+            }
         }
         return new StructureSummaryState(structures);
     }
 
     public static StructureSummaryState getOrCreate(ServerWorld world) {
         return world.getPersistentStateManager().getOrCreate(StructureSummaryState::readNbt, () -> {
-            StructureSummaryState state = new StructureSummaryState(new HashSet<>());
+            StructureSummaryState state = new StructureSummaryState(new HashMap<>());
             state.markDirty();
             return state;
         }, STATE_KEY);
@@ -62,5 +98,9 @@ public class StructureSummaryState extends PersistentState {
     public static void onChunkLoad(ServerWorld world, Chunk chunk) {
         StructureSummaryState state = StructureSummaryState.getOrCreate(world);
         chunk.getStructureStarts().forEach((structure, start) -> state.addStructure(world, start));
+    }
+
+    public record StructureKey(RegistryKey<Structure> key, StructureType<?> type) {
+
     }
 }
